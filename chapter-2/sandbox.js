1094ef2509cea894cc851b9e33d298a3509b17f9
Daniel Mintz <daniel_mintz@yahoo.co.uk>
2019-07-16 08:08:30 +0100

    strings

---
+++ chapter-2/sandbox.js
@@ -0,0 +1,45 @@
+// strings
+console.log('Hello World');
+let email = 'daniel.mintz@yahoo.co.uk';
+console.log(email);
+
+
+// string concatenation
+let firstName = 'Daniel';
+let lastName = 'Mintz';git 
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(fullName[1]);
+
+// string length
+console.log(fullName.length);
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+
+
+// alert('Coding Time');
+
+// console.log(1);
+
+// let age = 49;
+
+// console.log(age);
+
+// let year = 2019;
+
+// console.log('age', age, 'todays', year);
+
+// const dateOfbirth = 1970;
+
+// console.log ('date of birth:', dateOfbirth);
+
+
+
+
+
