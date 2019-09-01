7f07bd0d08344f182ca666f744c9ec465a2599b2
Daniel Mintz <daniel_mintz@yahoo.co.uk>
2019-09-01 10:17:39 +0100

    sadsa

--- Chapter-12-Asynchronous/9 Asynch and await  /sandbox.js
+++ Chapter-12-Asynchronous/9 Asynch and await  /sandbox.js
@@ -11,27 +11,49 @@
 //   return dataLuigi;
 // };
 
-// CHAINING
-//=============================================
+
 const getTodos =  async () => { 
 
-    const responseLuigi = await fetch('luigi.json');
-    
-    if(responseLuigi.status !== 200) {
-      throw new Error('cannot fetch the data');
-    }
+  const responseLuigi = await fetch('luigi.json');
+  
+  if(responseLuigi.status !== 200) {
+    throw new Error('cannot fetch the data');
+  }
 
-    const dataLuigi = await responseLuigi.json();
-    
+  const dataLuigi = await responseLuigi.json();
 
-    return dataLuigi;
-    
+  return dataLuigi
+  
 };
 
 getTodos()
-.then(dataLuigi => console.log('resolved:', dataLuigi))
+.then(dataLuigi => console.log('resolved1:', dataLuigi))
 .catch(err => console.log('rejected', err.message))
+// CHAINING (Doesnt work :(
+//=============================================
+// const getTodos =  async () => { 
+
+//     const responseLuigi = await fetch('luigi.json');
+    
+//     // if(responseLuigi.status !== 200) {
+//     //   throw new Error('cannot fetch the data');
+//     // }
+
+//     const dataLuigi = await responseLuigi.json();
+
+
+//     const responseMario = await fetch('mario.json');
+//     const dataMario = await responseMario.json();
+
+//      return dataLuigi, dataMario
+    
+// };
+
+// getTodos()
+// .then((dataLuigi,dataMario) => console.log('resolved1:', dataLuigi, 'resolved 2:', dataMario))
+// .catch(err => console.log('rejected', err.message))
  
+
 
 
   // Maybe how you Chain (TBC)
