13cc2c05e16253b616592d3da356d274421fed81
Daniel Mintz <daniel_mintz@yahoo.co.uk>
2019-10-01 14:17:52 +0100

    class

--- Chapter-18 realtimeChatRoom/scripts/chat.js
+++ Chapter-18 realtimeChatRoom/scripts/chat.js
@@ -9,6 +9,8 @@ class Chatroom {
         this.username = username;
         this.chats = db.collection('chats')
     }
+
+// adding new chat documents
 async addChat(message){
     //format a chat object
     const now = new Date();
@@ -24,12 +26,32 @@ async addChat(message){
     return response;
 
 }
+//real time listener to get new chats
+getChats(callback) {
+this.chats
+    .where('room', '==', this.room)
+    .orderBy('created_at')
+    .onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(change => {
+           if(change.type === 'added') {
+               //update the ui
+             callback(change.doc.data())
+           }
+
+        })
+});
+}
 }
 
-const chatroom = new Chatroom('gaming', 'shaun');
-chatroom.addChat('hello eveyone')
-    .then(()=> {
-        console.log('chat added')
-    }).catch((err) => {
-        console.log(err)
-    });
+const chatroom = new Chatroom('general', 'luigi');
+chatroom.getChats((data) => {
+    console.log(data)
+});
+
+// chatroom.addChat('hello eveyone')
+//     .then(()=> {
+//         console.log('chat added')
+//     }).catch((err) => {
+//         console.log(err)
+//     });
+
