786608b33d7073bcafb693da5553f4cb1c4f1fa4
Daniel Mintz <daniel_mintz@yahoo.co.uk>
2019-10-13 17:56:00 +0100

    watching webpack

--- Chapter20/src/index.js
+++ Chapter20/src/index.js
@@ -1,6 +1,13 @@
 import {styleBody, addTitle} from './dom';
+import users, { getPremUsers }  from './data';
+
+const premUsers = getPremUsers(users);
+
+console.log(users, premUsers);
 
 console.log('index file');
+
+console.log('test');
 
 styleBody();
 addTitle('hello from the Dom file');
