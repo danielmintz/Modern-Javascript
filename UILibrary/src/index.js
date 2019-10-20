291d99b7153a02d41fb90c5ebe53f7138b31b9de
Daniel Mintz <daniel_mintz@yahoo.co.uk>
2019-10-20 12:17:49 +0100

    dropdowns in the UI library

--- UILibrary/src/index.js
+++ UILibrary/src/index.js
@@ -1,8 +1,19 @@
 import Tooltip from './ninja.ui/tooltip';
+import Dropdown from './ninja.ui/dropdown';
+import './ninja.ui/styles/main.css';
 import './ninja.ui/styles/tooltip.css';
+import './ninja.ui/styles/dropdown.css';
 
 //create a tooltip
 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
 
 tooltip.init()
+
+//create a dropdown
+const dropdowns = document.querySelectorAll('.dropdown')
+
+dropdowns.forEach(dropdown => {
+    const instance = new Dropdown(dropdown);
+    instance.init()
+})
